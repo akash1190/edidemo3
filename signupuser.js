caf0f2b1f7d7f74a0f78f46779e456259f721870
akash1190 <38752437+akash1190@users.noreply.github.com>
2022-09-15 10:45:40 +0530

    Delete signupuser.js

--- signupuser.js
+++
@@ -1,7 +0,0 @@
-const sql = require("mssql");
-const signupuserSchema= sql.create({
-First_Name:String,
-Email_Id:String,
-Password:String
-});
-module.exports = sql.model("EDIA_LoginTable",signupuserSchema);
