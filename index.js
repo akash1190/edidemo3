7192b39828b6fbd4dc7d59f5165f1cb0c817a796
akash1190 <38752437+akash1190@users.noreply.github.com>
2022-09-15 17:29:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,12 @@ const app = express()
 const PORT = process.env.PORT || 3001
 
 app.use(express.static(path.join(__dirname + "/public")))
+app.get('*', function(_, res) {
+  res.sendFile(path.join(__dirname, '/public/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 
 app.listen(PORT)
